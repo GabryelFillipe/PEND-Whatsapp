06961bd50ade331c28bc6914c6027a8833476444
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-10-27 11:15:54 -0300

    Iniciando processo de desenvolvimento da logica do projeto

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 'use script'
+
+
+async function carregarContatos(user) {
+    
+    const url = ` https://pbe-api-whatsapp.onrender.com/v1/whatsapp/user/contatos/${user}`
+
+}
+function criarContatos(contatos){
+
+}
+
+async function carregarUsuarios() {
+    
+    const url = `https://corsproxy.io/?url=https://pbe-api-whatsapp.onrender.com/v1/whatsapp/users`
+    const response = await fetch(url)
+    const dados = await response.json()
+
+    return dados
+}
+
+async function criarModalLogin(){
+
+    const dados = await carregarUsuarios()
+    const users = dados.users
+}
+criarModalLogin()
